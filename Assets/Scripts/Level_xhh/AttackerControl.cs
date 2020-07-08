a2b73b96f738a3ddfc62ba8512d107b44fbf6995
xuhuihuang <xuhuihuang@xiaomi.com>
2020-07-08 20:33:05 +0800

    fix: 初步完成攻击者的移动跟随

---
+++ Assets/Scripts/Level_xhh/AttackerControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerControl : MonoBehaviour
+{
+    [Header("跟随速度")]
+    public float FollowSpeed;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private GameObject targetObj;
+    private GameObject heroFollowPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myRigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.targetObj)
+        {
+            this.followTarget();
+        }
+    }
+
+    private void followTarget()
+    {
+        float distance = (this.targetObj.transform.position - this.transform.position).magnitude;
+        Vector2 direction = (this.targetObj.transform.position - this.transform.position).normalized;
+
+        this.myRigidbody2D.velocity = Vector2.Lerp(this.myRigidbody2D.velocity, direction * this.FollowSpeed * distance, this.FollowSpeed);
+    }
+
+    public void initAttacker(GameObject heroFollowPoint)
+    {
+        this.heroFollowPoint = heroFollowPoint;
+        this.targetObj = heroFollowPoint;
+    }
+}
---
+++ Assets/Scripts/Level_xhh/HeroAttackerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAttackerControl : MonoBehaviour
+{
+    public AttackerControl Attacker;
+
+    public GameObject HeroAttackerFollowPart;
+
+
+    private AttackerControl nowAttacker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.nowAttacker = Instantiate<AttackerControl>(this.Attacker, this.HeroAttackerFollowPart.transform.position, this.Attacker.transform.rotation);
+        this.nowAttacker.initAttacker(this.HeroAttackerFollowPart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
