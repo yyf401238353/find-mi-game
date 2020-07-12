216b953b9a17f1ffc63e93da32406b44ec7c769b
Yifan Yang <yangyifan1@xiaomi.com>
2020-07-13 01:21:28 +0800

    add: add platform and hero move

--- Assets/Scripts/Level_yyf/HeroController.cs
+++ Assets/Scripts/Level_yyf/HeroController.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class HeroController : MonoBehaviour
 {
-    public float speed;
+    Rigidbody2D rigidbody2d;
     bool isJump;
     bool isFall;
-    float horizontal;
+    public float jumpSpeed = 8.0f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // input controll
+        Vector2 speedNow = rigidbody2d.velocity;
+        if(isJump && speedNow.y == 0)
+        {
+            isFall = true;
+            isJump = false;
+        } else if(isFall && speedNow.y == 0)
+        {
+            isFall = false;
+        }
+
+
+
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
-            horizontal = -1.0f;
+            speedNow.x = -2.0f;
         }
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            horizontal = 1.0f;
+            speedNow.x = 2.0f;
         }
         if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) {
-            horizontal = 0.0f;
+            speedNow.x = 0.0f;
         }
-
+        if (Input.GetKey(KeyCode.Space) && !isFall && !isJump)
+        {
+            speedNow.y = jumpSpeed;
+            isJump = true;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.Space))
+        {
+            rigidbody2d.velocity = speedNow;
+        }
         //Input.GetKeyDown(KeyCode.W);
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        transform.position = position;
     }
 }
