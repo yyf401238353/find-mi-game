22020af561d12f037941fdcfa29eee2f17489501
Yifan Yang <yangyifan1@xiaomi.com>
2020-07-28 00:55:30 +0800

    add: add shield, add air wall follow, add win end

---
+++ Assets/Scripts/Level_yyf/TeleportorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportorController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        HeroController controller = other.GetComponent<HeroController>();
+        if (controller != null)
+        {
+            controller.GameWin();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
